d26403e0c6a4f22b15bea27e2c34606d6afcc302
Nimit Agrawal <agrawalnimit03@gmail.com>
2025-09-26 09:55:28 +0530

    intial commit

---
+++ professional_setup/src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"  // helps in filehandling
+
+cloudinary.config({ 
+        cloud_name: 'dmwkfxidu', 
+        api_key: '716553891278935', 
+        api_secret: '<your_api_secret>'
+}) // copy pasted from cloudinary
