dd574820b2b81a9ee01f52435ee8eb992dc05a43
Nimit Agrawal <agrawalnimit03@gmail.com>
2025-09-29 16:09:54 +0530

    router and controller

--- professional_setup/src/app.js
+++ professional_setup/src/app.js
@@ -12,7 +12,13 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))   
 app.use(express.urlencoded({extended:true , limit: "16kb"}))
 app.use(express.static("public"))               // these all are basic configuration
-
 app.use(cookieParser())
+
+// routes import
+import userRouter from './routes/user.routes.js'  // we can give our name when only the export is default
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+//http://localhost:8000/api/v1/users/register
 
 export default app
