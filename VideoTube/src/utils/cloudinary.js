e6056e46bf5011795de9c8a38a902873ffc72130
NimitAgrawal2003 <agrawalnimit03@gmail.com>
2025-12-29 08:30:26 +0530

    Update cloudinary.js
    initial commit

--- VideoTube/src/utils/cloudinary.js
+++ VideoTube/src/utils/cloudinary.js
@@ -5,8 +5,7 @@ cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET
-}); // copy pasted from cloudinary
-
+}); 
 const uploadOnCloudinary = async (localFilePath) => {
         try{
                 if(!localFilePath){
@@ -15,13 +14,13 @@ const uploadOnCloudinary = async (localFilePath) => {
                 const response = await cloudinary.uploader.upload(localFilePath,{
                         resource_type:"auto"
                 })
-                // file has been uploaded successful
+                
                 console.log("file is uploaded on cloudinary",response.url);
                 fs.unlinkSync(localFilePath)
                 return response;
 
         }catch (error) {
-                fs.unlinkSync(localFilePath)   // remove locally saved temporary file as the upload operation got failed
+                fs.unlinkSync(localFilePath)  
                 return null;
         }
 }
