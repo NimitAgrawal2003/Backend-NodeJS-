847b99336aa019d36ef4535428c31bbb01ad3357
NimitAgrawal2003 <agrawalnimit03@gmail.com>
2025-12-29 08:31:26 +0530

    Update asyncHandler.js

--- VideoTube/src/utils/asyncHandler.js
+++ VideoTube/src/utils/asyncHandler.js
@@ -9,26 +9,4 @@ export { asyncHandler };
 
 
 
-/*The asyncHandler utility is used to wrap asynchronous route handlers in Express.
-Its main purpose is to catch errors from async functions and pass them to Express’s error-handling middleware, so you don’t need to write try/catch in every route.
 
-Benefits:
-
-Simplifies async code in controllers.
-Prevents unhandled promise rejections.
-Centralizes error handling. */
-
-/* or 
-
-const asyncHandler = (fn) => async (req,res,next) => {
-    try{
-    await fn(req,res,next)
-    } catch(error){
-     res.status(err.code) || 500).json({
-        success:false,
-        message:err.message
-     })
-     }
-    }
-
-    */
