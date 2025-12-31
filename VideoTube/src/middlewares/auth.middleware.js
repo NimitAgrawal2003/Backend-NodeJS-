2f6333990481bce41e85a3cd42312ebc66eaec4b
NimitAgrawal2003 <agrawalnimit03@gmail.com>
2025-12-31 10:48:39 +0530

    Fix formatting and comments in auth middleware

--- VideoTube/src/middlewares/auth.middleware.js
+++ VideoTube/src/middlewares/auth.middleware.js
@@ -5,14 +5,14 @@ import { User } from "../models/user.model.js";
 
 export const  verifyJWT = asyncHandler(async(req,res,next)=>{
    try {
-     // req got access of cokies from app.js where we have written app.use(cookieParser())
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")  // may or may get beacuse if user is using mobile it will send header
+     
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")  
     
     if(!token) {
      throw new ApiError(401, "Unauthorized request")
     }
  
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) // we neeed secret to decode it
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) 
  
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     if(!user){
